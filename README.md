aba5bd9aa04e33c7fc43844f504039975594de14
Takeshi Ueda <uedatakeshi@gmail.com>
2015-10-05 11:15:10 +0900

    Modify require fullcalendar.js

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ Creating applications with [Electron Document]( http://electron.atom.io/docs/v0.
 1. `git clone https://github.com/uedatakeshi/panels.git`.
 2. `cd panels`.
 3. `bower install`.
+3. And then run application by `electron ../panels`.
 
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-//'use strict';
+'use strict';
 
 window.jQuery = window.$ = require('./bower_components/jquery/dist/jquery.js');
 window.moment = require('./bower_components/moment/moment.js');
-//require('./bower_components/fullcalendar/dist/fullcalendar.js');
